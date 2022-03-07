fc88e076a099037840d01c6ed3ba5bd3342611d9
Benoit Lajeunesse <>
2022-03-07 14:43:01 -0500

    renamed Application file

--- src/main/java/com/blajeunesse/springboot/helloworld/HelloWorldApplication.java
+++ src/main/java/com/blajeunesse/springboot/helloworld/HelloWorldApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorldAppApplication {
+public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldAppApplication.class, args);
+		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
 }
