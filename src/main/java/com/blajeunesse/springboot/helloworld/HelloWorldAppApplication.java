5994fcd9fcb248c1acfdac372aaf7c8f747cdcac
blajeunesse <34555343+blajeunesse@users.noreply.github.com>
2022-03-07 14:33:49 -0500

    Add files via upload

---
+++ src/main/java/com/blajeunesse/springboot/helloworld/HelloWorldAppApplication.java
@@ -0,0 +1,13 @@
+package com.blajeunesse.springboot.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blajeunesse/springboot/helloworld/HelloWorldAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blajeunesse.springboot.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
