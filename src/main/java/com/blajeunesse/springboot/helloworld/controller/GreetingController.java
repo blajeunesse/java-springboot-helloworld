560d4550aef92a830ff3c3e02757f666954615d6
Benoit Lajeunesse <>
2022-03-08 15:26:38 -0500

    standardizing projects

---
+++ src/main/java/com/blajeunesse/springboot/helloworld/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package com.blajeunesse.springboot.helloworld.controller;
+
+import com.blajeunesse.springboot.helloworld.dto.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting hello(@RequestParam(value = "name", defaultValue = "Thomas") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
